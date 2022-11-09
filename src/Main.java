12977dd104e4e709c30dab42dc340c3b22341765
Alex <loalantref@hmail.com>
2022-11-09 22:18:22 +1200

    окончательный

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,39 @@ public class Main {
               System.out.println(cat);
         System.out.println(dog);
         System.out.println(papper);
+        System.out.println("_____________________________________________________________________________");
+        System.out.println("Новое задание");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        System.out.println("_____________________________________________________________________________");
+        System.out.println("Новое задание");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        System.out.println("_____________________________________________________________________________");
+        System.out.println("Новое задание");
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+        var summaryWeight = firstBoxerWeight + secondBoxerWeight;
+        System.out.println("Общий вес боксеров " + summaryWeight + " кг");
+        var diffWeight = secondBoxerWeight  %  firstBoxerWeight;
+        System.out.println("Разница в весе " + diffWeight + " кг");
+
+
+
+
+
+
+
+
     }
 }
