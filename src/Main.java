bcbccbb79146c24e6bc6acd942645d69a814fa5b
Alex <loalantref@hmail.com>
2022-11-09 21:25:17 +1200

    вычитание

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
+        var dog = 8.0;
         var cat = 3.6;
         var papper = 763789;
         dog = dog + 4;
         cat = cat +4;
         papper = papper + 4;
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        papper = papper - 7639;
               System.out.println(cat);
         System.out.println(dog);
         System.out.println(papper);
